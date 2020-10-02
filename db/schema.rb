4066bcf1e07abf2db0f787b5691537c6af71e128
Danny Ramos <danramos21@gmail.com>
2020-10-02 15:27:59 -0600

    migration for searches table plus schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_10_02_210354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "searches", force: :cascade do |t|
+    t.text "query_params"
+    t.string "raw_url"
+    t.string "api_key"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
