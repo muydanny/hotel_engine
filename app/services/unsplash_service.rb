003e3747733641d78558d749eb05d328a169cd9b
Danny Ramos <danramos21@gmail.com>
2020-10-03 14:31:42 -0600

    installed faraday and created service

---
+++ app/services/unsplash_service.rb
@@ -0,0 +1,20 @@
+class UnsplashService
+
+  def get_image(location)
+    response = conn.get("/search/photos") do |req|
+      req.params["query"] = "#{location}"
+    end
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  BASE_URL = "https://api.unsplash.com".freeze
+
+  def conn
+    Faraday.new(BASE_URL) do |req|
+      req.params["client_id"] = ENV["UNSPLASH_KEY"]
+      req.headers["Accept-Version"] = 'v1'
+    end
+  end
+end
