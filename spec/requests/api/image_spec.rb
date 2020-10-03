92d21cae930bba1bc7cb1bcf73f710e77edbf9fe
Danny Ramos <danramos21@gmail.com>
2020-10-03 14:30:29 -0600

    modified test description

--- spec/requests/api/image_spec.rb
+++ spec/requests/api/image_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe "Image of search API" do
-  it "can get a background image for a city" do
+  it "can get an image for a city" do
 
-    get "/api/v1/background?location=denver,co"
+    get "/api/v1/image?location=denver,co"
     expect(response).to be_successful  
     body = JSON.parse(response.body)
     expect(body["data"]["attributes"]["location_background_image"]).to be_a(String)
