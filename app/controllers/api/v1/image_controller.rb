598c44065a801d32ffbbac5b94f12e7b0abfc4a2
Danny Ramos <danramos21@gmail.com>
2020-10-07 13:25:58 -0600

    if search exists hits db instead of api

--- app/controllers/api/v1/image_controller.rb
+++ app/controllers/api/v1/image_controller.rb
@@ -1,13 +1,31 @@
 class Api::V1::ImageController < ApplicationController
 
   def show
-    image = UnsplashService.new.get_image(location_params[:location])
-    render json: UnsplashSerializer.new(Image.new(image)).serialized_json
+    search = Search.find_by(query_params: organized_params)
+    unless search 
+      search = Search.create(query_params: organized_params)
+      UnsplashService.new.get_images(search)
+    end
+
+    render json: UnsplashSerializer.new(search.results).serialized_json
   end
 
   private
   
-  def location_params
-    params.permit(:location)
+  def search_params
+    params.permit(:search)
   end
+
+  def query_params
+    search_params[:search].split(',')
+  end
+
+  def organized_params
+    query_params.reduce({}) do |collector, term|
+      split = term.split('=')
+      collector[split.first] = split.last
+      collector
+    end
+  end
+    
 end
