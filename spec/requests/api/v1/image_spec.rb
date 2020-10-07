acb5e3d34ab78c009deeb15ffbbd6b41bdbfa741
Danny Ramos <danramos21@gmail.com>
2020-10-07 13:23:12 -0600

    modified spec

--- spec/requests/api/v1/image_spec.rb
+++ spec/requests/api/v1/image_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 describe "Image of search API" do
   it "can get an image for a city" do
 
-    get "/api/v1/image?location=denver,co"
+    get "/api/v1/image?search=location=denver co,season=winter"
     expect(response).to be_successful  
     body = JSON.parse(response.body)
-    expect(body["data"]["attributes"]["location_image"]).to be_a(String)
+    
+    expect(body["data"][0]["attributes"]["image_url"]).to be_a(String)
+    expect(body["data"].count).to eq(10)
   end 
 
-  it "returns an error with bad request" do 
+  # it "returns an error with bad request" do 
 
-    post 
-  end
+  #   post 
+  # end
 end
