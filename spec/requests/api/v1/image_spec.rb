c767c7dddb19d37fee783d365b727e23068a0b3b
Danny Ramos <danramos21@gmail.com>
2020-10-09 15:55:07 -0600

    new specs for sorting and filtering

--- spec/requests/api/v1/image_spec.rb
+++ spec/requests/api/v1/image_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe "Image of search API" do
   it "can get an image for a city" do
-
     get "/api/v1/image?search=denver co winter"
     expect(response).to be_successful  
     body = JSON.parse(response.body)
@@ -12,7 +11,6 @@ describe "Image of search API" do
   end 
 
   it "can access db instead of api if search terms exist in the database" do 
-    # create search record wuth qp you pass thru 
     search = create(:search, :with_results)
     get "/api/v1/image?search=summer beach"
     expect(response).to be_successful
@@ -21,5 +19,23 @@ describe "Image of search API" do
     expect(body["data"].count).to eq(search.results.count)
   end
 
-  
+  # it "gives a 204 if the search parameter is missing" do 
+  #   get "/api/v1/image?denver co winter"
+  #   expect(response).to be_successful
+  #   body = JSON.parse(response.body)
+  #   require 'pry'; binding.pry
+
+  # end
+
+  it "provides the ability to sort the responses" do 
+    get "/api/v1/image?search=summer beach&sort= "
+    expect(response).to be_successful
+    body = JSON.parse(response.body)
+
+    require 'pry'; binding.pry
+  end
+
+  # it "provides the ability to filter the responses" do 
+  #   get "/api/v1/image?search=summer beach&filter="
+  # end
 end
