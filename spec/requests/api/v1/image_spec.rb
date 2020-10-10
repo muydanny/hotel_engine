825bd5d1a1031122ccd0798f37c14d47b5d47afd
Danny Ramos <danramos21@gmail.com>
2020-10-09 18:08:38 -0600

    filter and sort specs

--- spec/requests/api/v1/image_spec.rb
+++ spec/requests/api/v1/image_spec.rb
@@ -19,23 +19,38 @@ describe "Image of search API" do
     expect(body["data"].count).to eq(search.results.count)
   end
 
-  # it "gives a 204 if the search parameter is missing" do 
-  #   get "/api/v1/image?denver co winter"
-  #   expect(response).to be_successful
-  #   body = JSON.parse(response.body)
-  #   require 'pry'; binding.pry
-
-  # end
-
-  it "provides the ability to sort the responses" do 
-    get "/api/v1/image?search=summer beach&sort= "
-    expect(response).to be_successful
+  it "gives a 204 if the search parameter is missing" do 
+    get "/api/v1/image?denver co winter"
     body = JSON.parse(response.body)
 
-    require 'pry'; binding.pry
+    expect(response).to be_successful
+    expect(body["info"]).to eq "No Results"
   end
 
-  # it "provides the ability to filter the responses" do 
-  #   get "/api/v1/image?search=summer beach&filter="
-  # end
+  it "provides the ability to sort the responses" do 
+    get "/api/v1/image?search=summer beach&sort=updated&dir=asc"
+    expect(response).to be_successful
+    
+    body = JSON.parse(response.body)
+    first = body["data"].first
+    first_date = DateTime.parse(first["attributes"]["updated"])
+    last = body["data"].last
+    last_date = DateTime.parse(last["attributes"]["updated"])
+    
+    expect(last_date > first_date).to be true
+  end
+
+  it "provides the ability to filter the responses" do 
+    color_code = "#dabed8"
+    search = create(:search, :with_results)
+    search.results.update_all(color_code: "#FFFFFF")
+    search.results.first.update(color_code: color_code)
+    
+    get "/api/v1/image?search=summer beach&filter=%23dabed8"
+
+    body = JSON.parse(response.body)
+    expect(body["data"].count).to eq 1
+    expect(body["data"].first["attributes"]["color_code"]).to eq "#dabed8"
+  end
+
 end
