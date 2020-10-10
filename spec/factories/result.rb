b6146c4be65584c07ffd87f23b1ac665d51a4d5e
Danny Ramos <danramos21@gmail.com>
2020-10-09 18:08:28 -0600

    faker for color codes and updated times

--- spec/factories/result.rb
+++ spec/factories/result.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do 
   factory :result do 
     image_url { "www.google.com" }
+    color_code { Faker::Color.hex_color }
+    updated { Faker::Time.between(from: DateTime.now - 30, to: DateTime.now, format: :default) }
     association :search, factory: :search
   end
 end
