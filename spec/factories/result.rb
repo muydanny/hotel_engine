49b963e407313c3e4d30a972488137aa0bfad3eb
Danny Ramos <danramos21@gmail.com>
2020-10-09 18:28:50 -0600

    changed factory to HE

--- spec/factories/result.rb
+++ spec/factories/result.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do 
   factory :result do 
-    image_url { "www.google.com" }
+    image_url { "https://www.hotelengine.com/" }
     color_code { Faker::Color.hex_color }
     updated { Faker::Time.between(from: DateTime.now - 30, to: DateTime.now, format: :default) }
     association :search, factory: :search
